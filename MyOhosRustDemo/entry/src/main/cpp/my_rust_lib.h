ed10d71c917da7c12538addf5530c0af9434d8f3
qixinbing <qixinbing@rongcloud.cn>
2024-01-30 10:10:45 +0800

    提供鸿蒙项目

---
+++ MyOhosRustDemo/entry/src/main/cpp/my_rust_lib.h
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+uint32_t rust_add(uint32_t a, uint32_t b);
+
+#if defined(__cplusplus)
+} // closing brace for extern "C"
+#endif // __cplusplus
